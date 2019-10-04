259faf117e5b20512353f0e408c89ce159cd0d9e
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2019-10-04 15:22:20 -0500

    initial commit

---
+++ src/main/java/com/edu/escuelaing/arep/AREPSecureLab/ArepSecureLabApplication.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.arep.AREPSecureLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArepSecureLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArepSecureLabApplication.class, args);
+	}
+
+}
